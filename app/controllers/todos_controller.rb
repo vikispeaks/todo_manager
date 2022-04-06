ffce01ebdda088a7beb49ed5ca4aeebfd80ea791
Vignesh Rajendran <vignesh.r.rajendran@oracle.com>
2022-04-06 23:50:09 +0530

    Created a cutom URL for viewing single Todo Item

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,10 @@
 class TodosController < ApplicationController
     def index
-      render plain: Todo.all.to_a
+      render plain: Todo.order(:due_date).map { |todo| todo.to_pleasent_string }.join("\n")
+    end
+    def show
+        todo_id = params[:id]
+        todo = Todo.find(todo_id)
+        render plain: todo.to_pleasent_string
     end
   end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+    def to_pleasent_string
+        is_completed = completed ? "[X]" : "[ ]"
+        "#{id}. #{due_date.to_fs(:long)} #{todo_text} #{is_completed}"
+    end
 end
