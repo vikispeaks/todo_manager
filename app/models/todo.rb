35b45d6b4adb5be419373d28148b6efd4ced1f08
Vignesh Rajendran <vignesh.r.rajendran@oracle.com>
2022-04-12 20:50:51 +0530

    Update References to map to ERB render for HTML from DB

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,32 @@
 class Todo < ActiveRecord::Base
+  def overdue?
+    due_date < Date.today
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def due_later?
+    due_date > Date.today
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
   def to_pleasent_string
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_fs(:long)} #{todo_text} #{is_completed}"
